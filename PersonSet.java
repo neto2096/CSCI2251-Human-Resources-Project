a97699b3c30b0af72460c864170fd0ad0e6aa4c6
Ernesto Morales Carrasco <neto@MacBook-Pro-de-Ernesto-2.local>
2025-06-10 20:02:25 -0600

    created PersonSet class; comments still needed

--- PersonSet.java
+++ PersonSet.java
@@ -1,3 +1,22 @@
-public class PersonSet {
-    
+import java.util.ArrayList;
+
+public class PersonSet implements PersonList {
+    private ArrayList<Person> personList;
+
+    public PersonSet() {
+        personList = new ArrayList<>();
+    }
+
+    public void add(Person chosePerson) {
+
+    }
+
+    public Person get(int index) {
+        if (index >= 0 && index < personList.size()) {
+            return personList.get(index);
+        } else {
+            System.out.println("Index " + index + " is out of bounds");
+            return null;
+        }
+    }
 }
