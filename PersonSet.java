32f29e6788d46a317d068866de324ea8ec1d3240
Ernesto Morales Carrasco <neto@MacBook-Pro-de-Ernesto-2.local>
2025-06-10 20:48:52 -0600

    Added overrides and toString; still needs comments

--- PersonSet.java
+++ PersonSet.java
@@ -1,16 +1,20 @@
 import java.util.ArrayList;
 
 public class PersonSet implements PersonList {
-    private ArrayList<Person> personList;
+    protected ArrayList<Person> personList;
 
     public PersonSet() {
         personList = new ArrayList<>();
     }
 
-    public void add(Person chosePerson) {
-
+    @Override
+    public void add(Person chosenPerson) {
+        if (chosenPerson != null && !personList.contains(chosenPerson)) {
+            personList.add(chosenPerson);
+        }
     }
 
+    @Override
     public Person get(int index) {
         if (index >= 0 && index < personList.size()) {
             return personList.get(index);
@@ -18,5 +22,14 @@ public class PersonSet implements PersonList {
             System.out.println("Index " + index + " is out of bounds");
             return null;
         }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuild = new StringBuilder();
+        for (Person person : personList) {
+            stringBuild.append(person.toString()).append("\n");
+        }
+        return stringBuild.toString();
     }
 }
