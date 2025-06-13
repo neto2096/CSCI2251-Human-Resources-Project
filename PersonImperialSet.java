9a23e593754ef24b36b63200290e99b8b67469d8
neto2096 <emoralescarrasco9700@gmail.com>
2025-06-12 20:55:11 -0600

    overrided toString method; accounts for units; still need comments

--- PersonImperialSet.java
+++ PersonImperialSet.java
@@ -4,10 +4,9 @@ public class PersonImperialSet extends PersonSet {
         if (chosenPerson != null) {
             // Create new Person with converted height and weight
             Person imperialPerson = new Person(
-                chosenPerson.getName(),
-                convertHeight(chosenPerson.getHeight()),
-                convertWeight(chosenPerson.getWeight())
-            );
+                    chosenPerson.getName(),
+                    convertHeight(chosenPerson.getHeight()),
+                    convertWeight(chosenPerson.getWeight()));
             // Add converted Person using PersonSet's add
             super.add(imperialPerson);
         }
@@ -21,5 +20,15 @@ public class PersonImperialSet extends PersonSet {
     private double convertWeight(double weight) {
         // 1 kg ~ 2.20462 pounds
         return Math.round(weight * 2.20462 * 100.0) / 100.0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuild = new StringBuilder();
+        for (Person person : personList) {
+            stringBuild.append(person.toString()).append("\n");
+        }
+        String header = "Name\tHeight (in)\tWeight (lb)";
+        return header + "\n" + stringBuild.toString();
     }
 }
